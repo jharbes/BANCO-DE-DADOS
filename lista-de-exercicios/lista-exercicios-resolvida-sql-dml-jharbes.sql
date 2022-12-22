b687b3d39b84e005f9a469cbf8bcc362e9b3ab0a
Jorge Harbes <jharbes@hotmail.com>
2022-12-22 14:33:14 -0300

    Update lista-exercicios-resolvida-sql-dml-jharbes.sql

--- lista-de-exercicios/lista-exercicios-resolvida-sql-dml-jharbes.sql
+++ lista-de-exercicios/lista-exercicios-resolvida-sql-dml-jharbes.sql
@@ -19,9 +19,16 @@ select count(*) as totalCursos from curso;
 
 -- iii. Listar todos os cursos que tenham grade no ano de 2020.
 
-SELECT g.ano, c.nome FROM grade g, curso c WHERE g.idCurso=c.idCurso;
+SELECT g.ano, c.nome FROM grade g, curso c WHERE g.idCurso=c.idCurso and g.ano=2020;
 
 
 
--- iv. Informar quantas grades o curso de Engenharia Inform´atica possui.
+-- iv. Informar quantas grades o curso de Engenharia Informática possui.
 
+select count(*) as totalRegistros from grade g, curso c where g.idCurso=c.idCurso and c.nome='Engenharia Informática';
+
+
+
+-- v. Contar quantas disciplinas existem na grade com idGrade 8.
+
+select count(*) as totalDisciplinas from grade g, grade_disciplina gd where gd.idGrade=8;
